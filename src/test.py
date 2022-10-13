411f0b3fbfcd4c4f778b4ca4169df46292c223ae
Maxi Bove <maxibove13@gmail.com>
2022-10-13 16:00:18 -0300

    added profiles plots

--- src/test.py
+++ src/test.py
@@ -13,6 +13,7 @@ import os
 import time
 
 # Third party modules
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torch import sqrt
@@ -27,6 +28,10 @@ from visualization.utils import calc_mse, plot_flow_field_comparison
 
 root_dir = os.path.join('data', 'preprocessed', 'test')
 
+plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=16)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=16)    # fontsize of the tick labels
+
 # Load config file
 with open('config.yaml') as file:
     config = yaml.safe_load(file)
@@ -34,6 +39,8 @@ with open('config.yaml') as file:
 CHANNELS = config['data']['channels']
 CLIM_UX = config['data']['figures']['clim_ux']
 CLIM_UY = config['data']['figures']['clim_uy']
+D = config['data']['wt_diam']
+SIZE = config['data']['final_size'][0]
 
 def test():
 
@@ -99,6 +106,7 @@ def test():
 
     fig_im = plt.figure(dpi=300) # fig for flow field comparison
     fig_err = plt.figure(dpi=300) # fig for error comparison
+    fig_prof = plt.figure(figsize=(10, 25), dpi=300) # fig for error comparison
     grid = ImageGrid(
         fig_im, 
         111, 
@@ -117,10 +125,18 @@ def test():
         share_all='True', 
         cbar_location='right', 
         cbar_mode='edge')
+    grid_prof = ImageGrid(
+        fig_prof, 
+        111, 
+        nrows_ncols=(4,7) if CHANNELS == 1 else (4,14),
+        axes_pad=(0.15, 0.70), 
+        share_all='False',
+        aspect=False,
+        cbar_mode='none')
 
         # Plot figure with images real and fake
     if CHANNELS == 1:
-        grid_ims = [
+        grid_ims = grid_prof_ims = [
             images[0][0], fakes[0][0], images[1][0], fakes[1][0],
             images[2][0], fakes[2][0], images[3][0], fakes[3][0]]
         grid_err_ims = [
@@ -140,6 +156,7 @@ def test():
             fakes[2][0]-images[2][0],
             fakes[3][0]-images[3][0]]
 
+    # flow field comparison
     # Iterate over images in grid
     for c, (ax, im) in enumerate(zip(grid, grid_ims)):
         
@@ -183,9 +200,10 @@ def test():
             cb = grid.cbar_axes[1].colorbar(im)
             cb.set_label("[ms$^{-1}$]", rotation=270, labelpad=10)
 
+    # error comparison
     for c, (ax, im) in enumerate(zip(grid_err, grid_err_ims)):
 
-        # Determine the image vmin and vmax
+        # rescale image
         im = im * (CLIM_UX[1]-CLIM_UX[0]) + CLIM_UX[0]
 
         # Create image
@@ -203,6 +221,52 @@ def test():
         ax.set_title(f"Case #{c+1}")
         ax.get_xaxis().set_visible(False)
 
+    # profiles
+    # x position
+    x_left = -D*config['data']['lim_around_wt'][0]
+    x_right = D*config['data']['lim_around_wt'][1]
+    x = np.linspace(x_left, x_right, num=SIZE)
+
+    # y position
+    y_bottom = -D*config['data']['lim_around_wt'][2]
+    y_top = D*config['data']['lim_around_wt'][3]
+    y = np.linspace(y_bottom, y_top, num=SIZE)
+
+    # iterate over grid rows
+    for i, ax_row in enumerate(grid_prof.axes_row):
+        # get images
+        im_real = grid_prof_ims[2*i]
+        im_synth = grid_prof_ims[2*i+1]
+        # rescale them
+        im_real = im_real * (CLIM_UX[1]-CLIM_UX[0]) + CLIM_UX[0]
+        im_synth = im_synth * (CLIM_UX[1]-CLIM_UX[0]) + CLIM_UX[0]
+        # iterate over grid columns
+        for j, ax in enumerate(ax_row):
+            
+            # get closest index to this position
+            x_index = np.argmin(abs((j-2)*D - x))
+
+            # get profiles from images
+            real_prof = im_real[:, x_index].cpu()
+            synth_prof = im_synth[:, x_index].cpu()
+
+            real_curve, = ax.plot(real_prof, y/D, c='k', ls='-', label='CFD')
+            synth_curve, = ax.plot(synth_prof, y/D, c='r', ls='-.', label='GAN')
+
+            ax.set_xlim(left=1, right=12)
+            ax.grid()
+            ax.set_title(f'{j-2}D')
+            ax.tick_params(direction='in')
+
+            if i == len(grid_prof.axes_row)-1 and j == 0:
+                ax.set_xlabel('$U_x$ [ms$^{-1}$]')
+            if j == 0:
+                ax.set_ylabel(f'$y/D$ - Case #{i+1}')
+
+
+    fig_prof.legend(handles=[real_curve, synth_curve], bbox_to_anchor=(0., 0., 0.9, 0.097), ncol=2, fontsize=
+'xx-large')
+
     # Add suptitle
     fig_im.suptitle(
         f"Flow field comparison\n"
@@ -218,6 +282,7 @@ def test():
     # Save figures
     fig_im.savefig(os.path.join('figures', 'image_comparison_test.png'), dpi=300, bbox_inches='tight')
     fig_err.savefig(os.path.join('figures', 'image_comparison_err_test.png'), dpi=300, bbox_inches='tight')
+    fig_prof.savefig(os.path.join('figures', 'profiles.png'), dpi=300, bbox_inches='tight')
 
     print(f"RMSE for testing data: {rmse[0]:.3f}\n")
 
